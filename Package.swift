86543deff002ce185604c174cafcdda77cecf974
Roger Cruz <rbaltazarc@gmail.com>
2020-01-03 17:03:17 -0500

    add Package.swift

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "ALCameraViewController",
+    platforms: [.iOS(.v8), .macOS(.v10_10), .tvOS(.v9)],
+    products: [
+        .library(
+            name: "ALCameraViewController",
+            targets: ["ALCameraViewController"])
+    ],
+    targets: [
+        .target(
+            name: "ALCameraViewController",
+            path: "DevCod/ALCameraViewController")
+    ]
+)
